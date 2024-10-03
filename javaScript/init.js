a5d17458d1973dac85154bde66d644e21752c53e
psikoo <hugohernandezmuelas03@gmail.com>
2024-10-03 20:01:08 +0200

    1

--- javaScript/init.js
+++ javaScript/init.js
@@ -1,3 +1,4 @@
 import { processCommand } from "./commands.js";
-document.getElementById("text").innerHTML = "psikoo@website:~$ "
+
+document.getElementById("text").innerHTML = "psikoo@website:~$ ";
 processCommand("banner");
